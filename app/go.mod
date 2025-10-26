f3c4c31ebd30c3cf5faf72fbdd5445eac7ba6c4d
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-10-26 17:57:30 +0900

    fix: ディレクトリ構成修正

---
+++ app/go.mod
@@ -0,0 +1,3 @@
+module SatohAyaka/leaving-match-experiment
+
+go 1.24.4
--- app/main.go
+++ app/main.go
@@ -5,23 +5,26 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"SatohAyaka/leaving-match-experiment/lib"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("取得したいデータを選んでください")
-	fmt.Print("1: 滞在ウォッチ")
-	fmt.Print("2: leaving-match")
+	fmt.Print("取得したいデータを選んでください\n")
+	fmt.Print("1: 滞在ウォッチ\n")
+	fmt.Print("2: leaving-match\n")
 
 	mode, _ := reader.ReadString('\n')
 	mode = strings.TrimSpace(mode) // 改行文字を削除
 
 	switch mode {
 	case "1":
+		lib.StayWatch()
 	case "2":
+		lib.LeavingMatch()
 	default:
-		fmt.Println("エラー: 無効な入力です。1または2を入力してください。")
-		os.Exit(1)
+		fmt.Println("エラー: 無効な入力です.1または2を入力してください.")
 	}
 }
